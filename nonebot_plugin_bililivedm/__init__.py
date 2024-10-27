ef575b59793373cbe8687beb8ca1f8387ef95981
STESmly <3549337307@qq.com>
2024-10-28 00:22:33 +0800

    初次上传

--- nonebot_plugin_bililivedm/__init__.py
+++ nonebot_plugin_bililivedm/__init__.py
@@ -20,12 +20,10 @@ __plugin_meta__ = PluginMetadata(
     usage="",
 
     type="application",
-    # 发布必填，当前有效类型有：`library`（为其他插件编写提供功能），`application`（向机器人用户提供功能）。
 
     homepage="https://github.com/STESmly/nonebot_plugin_bililivedm",
-    # 发布必填。
 
-    supported_adapters={},
+    supported_adapters=None,
 )
 
 session: Optional[aiohttp.ClientSession] = None
